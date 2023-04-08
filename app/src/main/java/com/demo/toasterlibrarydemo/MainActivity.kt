5871e8d2826d7895eda01cd9cb748725a8f09544
Dilip Kumar Alampally <dilip.kumar.alampally@hexagon.com>
2023-04-08 15:27:46 -0700

    Initial commit

---
+++ app/src/main/java/com/demo/toasterlibrarydemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.demo.toasterlibrarydemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ toaster/src/main/java/com/demo/toaster/ToasterMessage.kt
@@ -0,0 +1,10 @@
+package com.demo.toaster
+
+import android.content.Context
+import android.widget.Toast
+
+class ToasterMessage {
+    fun showToast(context: Context, message: String) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+}
